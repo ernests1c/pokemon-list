5c67b4644afed7428c335fcbb2ed2e3ee6cd68fa
ernests1c <149561992+ernests1c@users.noreply.github.com>
2024-02-21 09:47:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -26,19 +26,35 @@ while True:
     choice = input("Enter your choice (1-6): ")
 
     if choice == '1':
+        pokemon_seq = int(input("Which pokemon would you like to choose?"))
+        print(pokemons[pokemon_seq])
         # https://www.w3schools.com/python/python_lists_access.asp
         pass
     elif choice == '2':
+        pokemons.sort()
+        print(pokemons)
         # https://www.w3schools.com/python/python_lists_sort.asp
         pass
     elif choice == '3':
+        pokemons.sort(reverse = True)
+        print(pokemons)
         # https://www.w3schools.com/python/python_lists_sort.asp
         pass
     elif choice == '4':
+        pokemon_search = str(input("By which letters would you like to search?"))
+        new_list = []
+        for x in pokemons:
+            if pokemon_search in x:
+               new_list.append(x)
+
+        print(new_list)
         # https://www.w3schools.com/python/python_lists_comprehension.asp
         # https://www.w3schools.com/python/ref_string_startswith.asp
         pass
     elif choice == '5':
+        pokemon_search2 = int(input("How many letters do you want your pokemon to be?"))
+        new_list2 = [x for x in pokemons if len(x) == pokemon_search2]
+        print(new_list2)
         # https://www.w3schools.com/python/python_lists_comprehension.asp
         pass
     elif choice == '6':
